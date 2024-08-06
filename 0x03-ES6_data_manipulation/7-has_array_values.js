9ed5e770e314b800f3bda5ec7dc39a898653e8e5
ahmedalaeldin <alaa.mido515@yahoo.com>
2024-08-06 20:17:00 +0000

    NewLine

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,10 @@
+const hasValuesFromArray = (set, array) => {
+  for (const item of array) {
+    if (!set.has(it[2;2Rem)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default hasValuesFromArray;
