326ab394e9d649c3a6c6da59dccca99ca5d8a789
ahmedalaeldin <alaa.mido515@yahoo.com>
2024-09-17 22:25:05 +0000

    NewRepo

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Ahmed Alaa <https://github.com/ahmedalaeldin>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
